8098cec81e25873104032ec4a3b9221bd8d4bc2d
Evgen959 <evgen.tm959@gmail.com>
2024-04-20 10:35:04 +0200

    new

--- Lesen 014/code/example_01/index.js
+++ Lesen 014/code/example_01/index.js
@@ -1,13 +1,12 @@
 // forEach
-// const ff = (a, b, c) => {    // a - элемент массива b -
+// const ff = (a, b, c) => {    // a - элемент массива b - индекс элемента c - весь массив
 //     console.log(`${a} + ${b} + ${c}`);
 // }
 const arrF = [1, 2, 3, 5, 4, 10, 6];
 console.log(arrF);
 // let b =0;
 // const ff = (a) => { 
-//     b = b + a;
-           
+//     b = b + a;       
 // }
 
 let arrayA = [];
@@ -15,26 +14,18 @@ const ff = (a) => {
     arrayA.push(a + 3); // пушим в пустой массив
 }
 
-
 arrF.forEach(ff); // запускает функцию для каждого элемента массива столько раз сколько элементов массива
-
-// console.log(arrayA);
-
-
 
 // map !!!
 const callbackForMap = (element) => {
     return element + 4;
 }
-
-const arrayAMap = arrF.map(callbackForMap);
+const arrayAMap = arrF.map(callbackForMap);// возвращает новый переделоный массив
 // console.log(arrayAMap);
-
 
 const callbackForMapA = (element) => {
     return element * element;
 }
-
 const arrayBMap = arrF.map(callbackForMapA);// возвращает новый переделоный массив
 // console.log(arrayBMap);
 
@@ -47,12 +38,11 @@ const callbackForFilter = (element) => {
     // }
     return element > 4 ? true: false;
 }
-
-const arrayAFilter = arrF.filter(callbackForFilter);
-// console.log(arrayAFilter);
+const arrayAFilter = arrF.filter(callbackForFilter); // возвращает новый массив отфильтрованный по заданым пораметрам
+console.log(arrayAFilter);
 
 // slice
-const arrayASliceResult = arrF.slice(0, 2);
+const arrayASliceResult = arrF.slice(0, 2); // возвращает новый массив, обрезает масив до указанного диопазона (указывающий элемент на конец диопазона не входит в него)
 // console.log(arrayASliceResult);
 
 // sort
@@ -66,20 +56,13 @@ const sallBackForSort = (a, b) => {
     // return a > b ? -1: 1;
 
     return b - a
-
 }
+const arrFcopy = [...arrF];  // доелаем копию!!!!! массива
+console.log(arrFcopy.sort(sallBackForSort)); // сортирует массив и !!!!!!!возвращает исходный!!!!!! массив уже отсортированый
 
-const arrFcopy = [...arrF];  // доелаем копию массива
-
-console.log(arrFcopy.sort(sallBackForSort));
-
-
-
-
-
+// классная работа: отсортировать массив из играков по росту "height"
 const sallBackForSortC = (object1, object2) => {
-    
-    return object1.height < object2.height ? -1: 1;
+        return object1.height < object2.height ? -1: 1;
 }
 const arrayC = [
   {"name": "John", "surname": "Doe", "height": 198, "effectiveness": 15.6},
@@ -88,5 +71,4 @@ const arrayC = [
   {"name": "Emily", "surname": "Davis", "height": 190, "effectiveness": 14.8},
   {"name": "Chris", "surname": "Wilson", "height": 205, "effectiveness": 20.1}
 ];
-
 console.log(arrayC.sort(sallBackForSortC));
