6f1fd2fc5e769e6d5266c7d949e134bd4201e6d5
Evgen959 <evgen.tm959@gmail.com>
2024-04-25 15:09:25 +0200

    newHomeworkL17

---
+++ Homework/Les017/example_04/scripts/index.js
@@ -0,0 +1,41 @@
+// Задание
+// Сделать страничку-таймер обратного отсчета. На страничке должно быть поле 
+// для ввода времени в секундах, кнопка "старт" и обратный отсчет 
+// должен начаться при нажатии на кнопку. Отсчет должен быть виден 
+// на страничке и обновляться каждую секунду. По окончании отсчета 
+// должен появиться тест "время вышло", или звуковое оповещение, 
+// таймер при этом должен остановиться.
+
+
+
+const numberInput = document.querySelector('input');
+const getButton = document.querySelector('Button');
+const spanElement = document.querySelector('span');
+let counter = 10;
+
+const setIntervalTime = Date.now();
+
+const timeHeandler = () => {
+        if (counter >= 0) {
+            spanElement.innerText = counter;
+            counter--;
+        } else {
+            clearInterval(intervalID);
+        }  
+    }
+const intervalID = setInterval(timeHeandler, 2000);
+const start = () => {
+    counter = numberInput.value;
+    console.log(counter);
+    setInterval(timeHeandler, 2000);    
+}
+getButton.addEventListener('click', start);
+
+
+
+
+
+//  
+// clearInterval(intervalID);
+// console.log('После запуска функции setInterval');
+
